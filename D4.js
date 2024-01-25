cab3d016647ed2948fd5bd755a863d3aca4927fb
Gianmarco Duilio <gianmarcoduilio@gmail.com>
2024-01-25 22:06:29 +0100

    Commit 1

--- D4.js
+++ D4.js
@@ -8,6 +8,7 @@ function area(l1, l2) {
   const areaRettangolo = l1 * l2;
   return areaRettangolo;
 }
+
 let l1 = 24;
 console.log(l1);
 
@@ -22,6 +23,7 @@ La funzione deve ritornare la somma dei due parametri, ma se il valore dei due p
 la loro somma moltiplicata per tre.
 */
 /* SCRIVI QUI LA TUA RISPOSTA */
+
 function crazySum(n1, n2) {
   if (n1 === n2) {
     const sommaX3 = (n1 + n2) * 3;
@@ -31,6 +33,7 @@ function crazySum(n1, n2) {
     return somma;
   }
 }
+
 let n1 = 8;
 let n2 = 2;
 console.log(crazySum(n1, n2));
@@ -41,6 +44,7 @@ console.log(crazySum(n1, n2));
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
 function crazyDiff(num1) {
   let risultato;
   if (num1 > 19) {
@@ -51,28 +55,30 @@ function crazyDiff(num1) {
     return risultato;
   }
 }
+
 let num1 = Math.floor(Math.random() * 30);
 
 console.log(crazyDiff(num1, 19));
+
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
+/* SCRIVI QUI LA TUA RISPOSTA */
+
 function boundary(n) {
-  if (n >= 20 && n <= 200) {
-    return true;
-  } else if ((n = 400)) {
+  if ((n >= 20 && n <= 100) || n === 400) {
     return true;
   } else {
     return false;
   }
 }
 
-let n = Math.floor(Math.random() * 1000);
+let n = Math.floor(Math.random() * 401);
+console.log(n);
 
 console.log(boundary(n));
 
-/* SCRIVI QUI LA TUA RISPOSTA */
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
  La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
@@ -81,6 +87,16 @@ console.log(boundary(n));
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function epify(stringa) {
+  if (stringa.startsWith("EPICODE")) {
+    return stringa;
+  } else {
+    return "EPICODE" + stringa;
+  }
+}
+
+console.log(epify(" Ciao"));
+
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
@@ -88,11 +104,29 @@ console.log(boundary(n));
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function check3and7(numeroPositivo) {
+  if (numeroPositivo > 0 && numeroPositivo % 3 === 0) {
+    return "il numero è multiplo di 3";
+  } else if (numeroPositivo > 0 && numeroPositivo % 7 === 0) {
+    return "il numero è multiplo di 7";
+  } else {
+    return "il numero non è multiplo né di 3 né di 7 o non è positivo";
+  }
+}
+
+console.log(check3and7(28));
+
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function reverseString(stringa2) {
+  return stringa2.split("").reverse().join("");
+}
+
+console.log(reverseString("EPICODE"));
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
@@ -105,11 +139,28 @@ console.log(boundary(n));
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
  della stringa originale.
 */
-
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function cutString(stringa4) {
+  const stringaDivisa = stringa4.split("");
+  return stringaDivisa.slice(1, -1).join("");
+}
+
+console.log(cutString("EPICODE"));
 
 /* ESERCIZIO 10
  Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function giveMeRandom(numeroN) {
+  let numeriRandom = [];
+  for (let i = 0; i < numeroN; i++) {
+    numeriRandom.push(Math.floor(Math.random() * 11));
+  }
+  return numeriRandom;
+}
+
+const risultato = giveMeRandom(Math.floor(Math.random() * 10));
+console.log(risultato);
